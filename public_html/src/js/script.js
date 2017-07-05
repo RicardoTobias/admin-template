68af347b3a624abe2a4de3c8bbbac6adb52903b8
ricardo <ricardosantostobias@yahoo.com.br>
2017-07-05 11:47:12 -0300

    - Inserting pages list and edit page on the menu aside

---
+++ public_html/src/js/script.js
@@ -0,0 +1,38 @@
+// tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+// btn check
+$('#radioBtn a').on('click', function () {
+    var sel = $(this).data('title');
+    var tog = $(this).data('toggle');
+    $('#' + tog).prop('value', sel);
+
+    $('a[data-toggle="' + tog + '"]').not('[data-title="' + sel + '"]').removeClass('active').addClass('notActive');
+    $('a[data-toggle="' + tog + '"][data-title="' + sel + '"]').removeClass('notActive').addClass('active');
+});
+
+//
+$("#input-id").fileinput({
+    showUpload: false,
+    previewFileType: 'any',
+    showCaption: false,
+    showClose: true,
+    language: 'pt-BR',
+    maxFileSize: 1500,
+    showBrowse: false,
+    browseOnZoneClick: true,
+    elErrorContainer: '#kv-avatar-errors-2',
+    msgErrorClass: 'alert alert-block alert-danger',
+    defaultPreviewContent: '<img src="http://localhost:8000/public_html/assets/img/upload/photo.png" alt="Seu avatar" style="width:100%">'+
+                            '<h6 class="text-muted" style="text-align: center;font-size: 14px;">Avatar</h6>',
+    allowedFileExtensions: ["jpg", "png", "gif"]
+});
+
+$('.selectpicker').selectpicker({
+    width: '100%'
+});
+
+
+
